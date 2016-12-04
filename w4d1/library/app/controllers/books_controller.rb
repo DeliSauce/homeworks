80b903826e5cfc27842e5a255861ad09f0a2c19a
DeliSauce <nycdelisauce@gmail.com>
2016-12-03 19:24:41 -0800

    added the latest homework files

---
+++ w4d1/library/app/controllers/books_controller.rb
@@ -0,0 +1,27 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    fail
+    new_book = Book.new(book_params)
+    new_book.save
+    redirect_to(books_url)
+  end
+
+  def destroy
+    book = Book.find_by(id: params[:id])
+    book.destroy
+    redirect_to(books_url)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+end
